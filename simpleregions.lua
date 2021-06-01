00dd4a72a794945f193cb500eeb12a5c776f36b7
Sudospective <avery@sudospective.net>
2021-06-01 06:27:24 -0500

    Update simpleregions.lua

--- simpleregions.lua
+++ simpleregions.lua
@@ -3,7 +3,7 @@ function region(t)
     local pn = t.plr or get_plr()
     if type(pn) == 'number' then pn = {pn} end
     for _, plr in ipairs(plr) do
-        P[pn]:SetHiddenRegions({{t[1], t[1] + t[2]}})
+        P[pn]:add_hidden_regions({{t[1], t[1] + t[2]}})
     end
     return region
 end
